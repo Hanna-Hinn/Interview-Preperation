d98553725b7153e811f3f47ff4fbce2b23e851d6
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2022-05-31 20:56:09 +0300

    Create Print2Darray.java

---
+++ Print2Darray.java
@@ -0,0 +1,54 @@
+//This Prints u a 2D array from left to right in the first row and then from right to left and so on.
+public class arrays {
+
+	//
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[][] twoD = { 
+				{ 1, 2, 3 }, 
+				{ 4, 5, 6 }, 
+				{ 7, 8, 9 } };
+		int i = 0;
+		int j = 0;
+		int counter = 0;
+		boolean direction = true;
+		while (true) {
+			
+			if ( i == twoD.length-1 && j == twoD[i].length-1 ) {
+				System.out.println(twoD[i][j]);
+				break;
+			}
+
+			counter++;
+      
+			if (j < twoD[i].length) {
+				System.out.println(twoD[i][j]);
+
+			}
+			
+			
+			if (direction) {
+				j++;
+				if(j==twoD[i].length) {
+					j = twoD[i].length-1;
+				}
+			}else {
+				j--;
+				if(j<0) {
+					j = j+1;
+				}
+			}
+
+			if (counter == twoD.length) {
+				direction = !direction;
+				counter = 0;
+				i++;
+			}
+
+			
+		}
+
+	}
+
+}
