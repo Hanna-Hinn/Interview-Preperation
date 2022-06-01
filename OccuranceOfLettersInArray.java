7e4e0a1dbdfe24c8d718ce57c27c795aafea73dd
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2022-06-01 09:40:31 +0300

    Create OccuranceOfLettersInArray.java

---
+++ OccuranceOfLettersInArray.java
@@ -0,0 +1,28 @@
+//How to calculate the occurance of characters using array (a-z) without the Captial letters
+public class Main {
+
+	public static void main(String[] args) {
+    
+    occuranceOfLetters("codebinarytest");
+    
+  }
+  
+  public static void occuranceOfLetters(String str) {
+		int[] arr = new int[26];
+		for(int i =0;i<arr.length;i++) {
+			arr[i] =0;
+		}
+		
+		for(int i=0;i<str.length();i++) {
+			char tmp = str.charAt(i);
+			int index = tmp%97;
+			arr[index]++;
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println( (char)(97+i)+" "+arr[i]);
+		}
+		
+	}
+  
+}
